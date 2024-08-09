d4625f01df70d1b77475fd5b4c003d8172dd37de
KoyamaShota <k023g1039@g.neec.ac.jp>
2024-08-09 11:22:46 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <Novice.h>
+#include <Game.h>
+#include <Camera.h>
+
+const char kWindowTitle[] = "GC2B_06_コヤマ_ショウタ";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, (int)Camera::GetKWindowWidth(), (int)Camera::GetKWindowHeight());
+
+	Game* game = new Game();
+
+	game->Main();
+
+	// ライブラリの終了
+	Novice::Finalize();
+	delete game;
+	return 0;
+
+}
